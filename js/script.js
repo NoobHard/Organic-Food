68dc77d581c1c768329766dd0e35d073abfd076d
NoobHard <106015255+NoobHard@users.noreply.github.com>
2024-05-30 18:47:29 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
 
-burger.addEventListener('click', () => {
-  nav.classList.toggle('open');
+burger.addEventListener('click', openMenu) 
+  function openMenu() {
+
+   nav.classList.toggle('open');
   burger.classList.toggle('active');
-});
+
+
+}
+  
